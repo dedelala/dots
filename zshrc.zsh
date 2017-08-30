59478a3a773886488cb629a25404a0d1b69b7c82
Dede Lamb <dede.lamb@myob.com>
2017-08-30 13:25:15 +1000

    show that i read the zshoptions

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,10 +1,25 @@
-setopt interactive_comments
-setopt extended_glob
-setopt auto_pushd
-setopt pushd_minus
-setopt pushd_ignore_dups
-setopt prompt_subst 
-setopt auto_menu # use menu completion
+setopt auto_menu # use menu completion after two tabs
+setopt auto_pushd # cd pushes to the dir stack
+setopt extended_glob # always
+setopt glob_complete # don't expand globs for completion
+setopt glob_star_short # ** = recurse. *** = recurse and follow sym
+setopt hist_ignore_all_dups # dups cause older history to be removed
+setopt hist_no_functions # function defs not stored
+setopt hist_no_store # "history" calls not stored
+setopt hist_reduce_blanks # trim pointless whitespace in hist
+setopt hist_verify # expand hist in-place
+setopt inc_append_history # append history as commands are entered
+setopt interactive_comments # allow comments in line editor
+setopt list_types # show filetype chars in completion
+setopt magic_equal_subst # file completion after =
+setopt numeric_glob_sort # sort number filenames by number in completion
+setopt pipe_fail # exit code is the left-most non-zero
+setopt prompt_subst # do expansions on PS
+setopt pushd_ignore_dups # dir stack ignore dups
+setopt pushd_minus # +/- swapped on dir stack
+setopt rc_expand_param # gooder string/array exansions
+# todo job control
+
 
 autoload -U compinit
 compinit
@@ -117,7 +132,7 @@ kcname() {
     s=$1
     shift
     n="$@[$(( $RANDOM % ${#@[@]} + 1 ))]"
-    ps |grep -E "kak.+$n.+$s" |grep -v grep && n=$(kcname $s ${@/$n})
+    ps |grep -E "kak.+$n.+$s" |grep -v grep && n=$(kcname $s "${@/$n}")
     echo $n
 }
 
