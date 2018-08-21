656a7d6b617be79e831122e6ba697fcb9a577af2
Dede Lamb <dede.lamb@myob.com>
2018-08-21 19:30:16 +1000

    ditch make

---
+++ dots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+die() { echo "failed: $*" >&2; exit 1; }
+
+cd "$(dirname "$(git rev-parse --absolute-git-dir)")" || die "dots root"
+
+cat zsh/init.zsh zsh/fn-*.zsh zsh/rc.zsh > "$HOME/.zshrc"
+
+mkdir -p "$HOME/.config/kak/colors" || die "kak dirs"
+cp dedelala.kak "$HOME/.config/kak/colors" || die "kak colors"
+cp kakrc "$HOME/.config/kak" || die "kakrc"
