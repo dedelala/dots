99356cc9a6dc349add42439a2bc05423756132b3
Dede Lamb <hey@dedela.la>
2018-11-18 14:02:42 +1100

    new world order

--- dots.sh
+++ dots.sh
@@ -4,8 +4,12 @@ die() { echo "failed: $*" >&2; exit 1; }
 
 cd "$(dirname "$0")" || die "dots root"
 
-cat zsh/init.zsh zsh/fn-*.zsh zsh/rc.zsh > "$HOME/.zshrc"
+cat zsh/init.zsh zsh/cdp.zsh zsh/rc.zsh > "$HOME/.zshrc" || die "zsh"
 
-mkdir -p "$HOME/.config/kak/colors" || die "kak dirs"
-./dedelala.kak.sh > "$HOME/.config/kak/colors/dedelala.kak" || die "kak colors"
-cp kakrc "$HOME/.config/kak" || die "kakrc"
+mkdir -pv "$HOME/.config/kak/colors" || die "kak dirs"
+kak/dedelala.kak.sh > "$HOME/.config/kak/colors/dedelala.kak" || die "kak colors"
+cp -v kak/kakrc "$HOME/.config/kak" || die "kakrc"
+
+for x in x/*; do
+        cp -v "$x" "$HOME/.$(basename "$x")"
+done
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,25 +1,25 @@
 # options
-setopt auto_menu # use menu completion after two tabs
-setopt auto_pushd # cd pushes to the dir stack
-setopt chase_links # cd resolves symlinks
-setopt extended_glob # always
-setopt glob_complete # don't expand globs for completion
-setopt glob_star_short # ** = recurse. *** = recurse and follow sym
-setopt hist_ignore_all_dups # dups cause older history to be removed
-setopt hist_no_functions # function defs not stored
-setopt hist_no_store # "history" calls not stored
-setopt hist_reduce_blanks # trim pointless whitespace in hist
-setopt hist_verify # expand hist in-place
-setopt inc_append_history # append history as commands are entered
-setopt interactive_comments # allow comments in line editor
-setopt list_types # show filetype chars in completion
-setopt magic_equal_subst # file completion after =
-setopt numeric_glob_sort # sort number filenames by number in completion
-setopt pipe_fail # exit code is the left-most non-zero
-setopt prompt_subst # do expansions on PS
-setopt pushd_ignore_dups # dir stack ignore dups
-setopt pushd_minus # +/- swapped on dir stack
-setopt rc_expand_param # gooder string/array exansions
+setopt auto_menu               # use menu completion after two tabs
+setopt auto_pushd              # cd pushes to the dir stack
+setopt chase_links             # cd resolves symlinks
+setopt extended_glob           # always
+setopt glob_complete           # don't expand globs for completion
+setopt glob_star_short         # ** = recurse. *** = recurse and follow sym
+setopt hist_ignore_all_dups    # dups cause older history to be removed
+setopt hist_no_functions       # function defs not stored
+setopt hist_no_store           # "history" calls not stored
+setopt hist_reduce_blanks      # trim pointless whitespace in hist
+setopt hist_verify             # expand hist in-place
+setopt inc_append_history      # append history as commands are entered
+setopt interactive_comments    # allow comments in line editor
+setopt list_types              # show filetype chars in completion
+setopt magic_equal_subst       # file completion after =
+setopt numeric_glob_sort       # sort number filenames by number in completion
+setopt pipe_fail               # exit code is the left-most non-zero
+setopt prompt_subst            # do expansions on PS
+setopt pushd_ignore_dups       # dir stack ignore dups
+setopt pushd_minus             # +/- swapped on dir stack
+setopt rc_expand_param         # gooder string/array exansions
 
 
 # exports
@@ -27,15 +27,11 @@ export HISTSIZE=10000
 export SAVEHIST=10000
 export HISTFILE=$HOME/.zsh_history
 export WORDCHARS='*?_-.[]~;!#$%^(){}<>' # characters considered to be part of a word (zle)
-export CDPATH=$HOME/.pj/.p
+export CDPATH=$HOME/.cdp/.p
 export PATH=$PATH:/usr/local/opt/go/bin:$HOME/go/bin
 export GOPATH=$HOME/go
-export GO111MODULE=on
 export EDITOR=kak
 export DOCKER_ID_USER="dedelala"
-export LSCOLORS="gafaBabacaxxxxxxxxxxxx"
-export AWS_DEFAULT_REGION="ap-southeast-2"
-export AWS_REGION="ap-southeast-2"
 
 
 # aliases
@@ -65,16 +61,8 @@ hash gmake &>/dev/null && alias make="gmake"
 
 
 # prompt
-export EMOS=(🐧 🐈 💖 🌈 🎀 🐉 🍄 👻 👒 👀 🌼 🎪 🍌 😱 🚧)
-export EMO=$EMOS[$((1 + $(tty |tr -d a-z/)))]
+export PS1='$(rack ps1)'
+export RPS1='$(rack rps1)'
+export PS2='$(rack ps2)'
 
-export PS1='%(0?;;💔%?)$(ps-host)$(ps-git)%F{015}%B%1~%b%f$(ps-br)$(ps-make)$(ps-docker)%B %b'
-export RPS1='$(ps-id)'
-
-export PS2='%B %b'
-export RPS2='$(ps-id)'
-
-
-# el crujido
-pj cd
-motd
+cdp cd || cdp be lala
