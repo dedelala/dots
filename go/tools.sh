66efcef84ee8dae1d95a222920da1d3d6db884f6
Dede Lamb <hey@dedela.la>
2018-11-18 19:00:26 +1100

    7

--- go/tools.sh
+++ go/tools.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 while read -r p; do
-        go get -u "$p"
+        if ! go get -u "$p"; then
+                echo ":( $p"
+                exit 1
+        fi
 done <<!
 github.com/stamblerre/gocode
 github.com/zmb3/gogetdoc
